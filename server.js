471a1e6ea2c191d1cf90b32b1e9b9ba784176fe3
Hector Zelaya <inge.zelaya@gmail.com>
2017-08-29 07:01:45 -0600

    first commit

---
+++ server.js
@@ -0,0 +1,55 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + '/index.html');
+});
+
+app.get('/client.js', function (req, res) {
+    res.sendFile(__dirname + '/client.js');
+});
+
+io.on('connection', function (socket) {
+    console.log('a user connected');
+
+    socket.on('create or join', function (room) {
+        console.log('create or join to room ', room);
+        
+        var myRoom = io.sockets.adapter.rooms[room] || { length: 0 };
+        var numClients = myRoom.length;
+
+        console.log(room, ' has ', numClients, ' clients');
+
+        if (numClients == 0) {
+            socket.join(room);
+            socket.emit('created', room);
+        } else if (numClients == 1) {
+            socket.join(room);
+            socket.emit('joined', room);
+        } else {
+            socket.emit('full', room);
+        }
+    });
+
+    socket.on('ready', function (room){
+        socket.broadcast.to(room).emit('ready');
+    });
+
+    socket.on('candidate', function (event){
+        socket.broadcast.to(event.room).emit('candidate', event);
+    });
+
+    socket.on('offer', function(event){
+        socket.broadcast.to(event.room).emit('offer',event.sdp);
+    });
+
+    socket.on('answer', function(event){
+        socket.broadcast.to(event.room).emit('answer',event.sdp);
+    });
+
+});
+
+http.listen(3000, function () {
+    console.log('listening on *:3000');
+});
